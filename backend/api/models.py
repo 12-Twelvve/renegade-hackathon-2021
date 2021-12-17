ca2d39257d38db2a3f79ddc2f7c3b6703e47cc6a
shreyaabh <41884465+shreyaabh@users.noreply.github.com>
2021-12-17 12:34:15 +0545

    merge jeevan

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "cities"
+
+class State(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "state"
+
+class Country(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "country"
